dfc38e1084a4e285a82d2671bfdf8ed6b0cd97cf
kjch9182-create <kjch9182@gmail.com>
2025-09-25 21:21:28 +0900

    2일차
    이동

---
+++ Assets/Script/Ship.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
+
+
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        float xOffset = hor * Time.deltaTime * speed;
+        float newXpos = transform.localPosition.x + xOffset;
+        float clampedXpos = Mathf.Clamp(newXpos, -xRange, xRange);
+
+        float yOffset = ver * Time.deltaTime * speed;
+        float newYpos = transform.localPosition.y + yOffset;
+        float clampedYpos = Mathf.Clamp(newYpos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
+    }
+}
